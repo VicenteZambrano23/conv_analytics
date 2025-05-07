080a0e598240a86aed259134bc2439a5162b145f
CarlosRamosCognizant <carlos.ramosillan@cognizant.com>
2025-05-07 16:01:19 +0000

    Update Rag Agent

--- RAG.py
+++ RAG.py
@@ -1,4 +1,5 @@
 import autogen
+import os
 from autogen import AssistantAgent
 from autogen.agentchat.contrib.retrieve_user_proxy_agent import RetrieveUserProxyAgent
 from config.config import AZURE_OPENAI_CONFIG
@@ -6,7 +7,7 @@ from chromadb.utils import embedding_functions
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-                api_key=AZURE_OPENAI_CONFIG,
+                api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
                 model_name="text-embedding-ada-002"
             )
 
@@ -24,17 +25,14 @@ ragproxyagent = RetrieveUserProxyAgent(
     name="ragproxyagent",
     retrieve_config={
         "task": "qa",
-        "docs_path": "/teamspace/studios/this_studio/conv_analytics/database/RAG_files/Example.md",  
+        "docs_path": ["/teamspace/studios/this_studio/conv_analytics/database/RAG_files/Example.md","/teamspace/studios/this_studio/conv_analytics/database/RAG_files/Example_2.md"] ,  
         "get_or_create": True,  # set to False if you don't want to reuse an existing collection
         "overwrite": False,  # set to True if you want to overwrite an existing collection      
-       # "custom_text_split_function": recur_spliter.split_text,
-       # "embedding_function": openai_ef,
+        "custom_text_split_function": recur_spliter.split_text,
+        #"embedding_function": openai_ef,
     },
 )
 
 assistant.reset()
-ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem="What is bluebolt?")
+ragproxyagent.initiate_chat(assistant, message=ragproxyagent.message_generator, problem="When are Cognizant bussiness units?")
 
-assistant.reset()
-userproxyagent = autogen.UserProxyAgent(name="userproxyagent")
-userproxyagent.initiate_chat(assistant, message="What is bluebolt?")
