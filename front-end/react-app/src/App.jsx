c2bbe6849194e873e312fe4a5e6f9e7fca216343
VicenteZambrano23 <vicente.zambrano@cognizant.com>
2025-05-19 08:17:05 +0000

    api changes

--- front-end/react-app/src/App.jsx
+++ front-end/react-app/src/App.jsx
@@ -30,12 +30,12 @@ function App() {
 
     if (chatStatus === "Chat ongoing" || chatStatus === "inputting") {
       // Send message request
-      apiEndpoint = "https://5008-01jt2vmajek84mhmqvahvjx08v.cloudspaces.litng.ai/api/send_message";
+      apiEndpoint = "https://5009-01jr7k2qz227qhygnkh9vjydzp.cloudspaces.litng.ai/api/send_message";
       requestBody = { message: content };
     } else {
 
       // Start chat request
-      apiEndpoint = "https://5008-01jt2vmajek84mhmqvahvjx08v.cloudspaces.litng.ai/api/start_chat";
+      apiEndpoint = "https://5009-01jr7k2qz227qhygnkh9vjydzp.cloudspaces.litng.ai/api/start_chat";
       requestBody = { ...initialChatRequest, message: content };
     }
 
@@ -63,7 +63,7 @@ function App() {
   // Function to fetch messages from the backend
   const fetchMessages = async () => {
     try {
-      const response = await fetch("https://5008-01jt2vmajek84mhmqvahvjx08v.cloudspaces.litng.ai/api/get_message");
+      const response = await fetch("https://5009-01jr7k2qz227qhygnkh9vjydzp.cloudspaces.litng.ai/api/get_message");
       if (!response.ok) {
         throw new Error("Failed to fetch messages");
       }
@@ -74,7 +74,7 @@ function App() {
         const messageContent = data.message.message;
 
         // Only add messages from 'User_Proxy' or 'sql_proxy'
-        if (messageUser === "User_Proxy" || messageUser === "sql_proxy" || messageUser === "graph_agent" || messageUser === "graph_executor" || messageUser === "query_agent"  || messageUser === "executor_query" || messageUser == "add_filter_agent" || messageUser == "add_filter_executor") {
+        if (messageUser === "User_Proxy" || messageUser === "sql_proxy" || messageUser === "graph_agent" || messageUser === "graph_executor" || messageUser === "RAG_executor" || messageUser === "query_agent"  || messageUser === "RAG_agent"  ||  messageUser === "executor_query" || messageUser == "add_filter_agent" || messageUser == "add_filter_executor") {
           const isUserProxy = messageUser === "User_Proxy";
           const isSqlProxy = messageUser === "sql_proxy";
           const isGraphAgent = messageUser === "graph_agent";
